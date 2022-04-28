2a48c0fe01f9ee8901f0407c040800e276ee82d3
LucaoMendes <lucas1999mendes@gmail.com>
2022-04-28 00:34:27 +0000

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import express, { Request , Response , NextFunction} from "express";
+
+const app = express();
+
+app.get('/status',(req: Request,res : Response,next : NextFunction) => {
+    console.log("Status")
+    res.status(200).send({
+        foo : 'bar'
+    })
+})
+
+app.listen(80,()=>console.log("Aplicação iniciada na porta 80"))
